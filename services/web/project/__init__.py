5d8f57e8c6ebef570a88c2456d14adb3960f7ff8
CharlieBravoCode <89343590+CharlieBravoCode@users.noreply.github.com>
2022-11-12 15:53:42 +0100

    static path "hello world" added

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify, send_from_directory
 
 
 app = Flask(__name__)
@@ -21,3 +22,7 @@ class User(db.Model):
 @app.route("/")
 def hello_world():
     return jsonify(hello="world")
+
+@app.route("/static/<path:filename>")
+def staticfiles(filename):
+    return send_from_directory(app.config["STATIC_FOLDER"], filename)
